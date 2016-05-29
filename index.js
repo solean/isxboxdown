53e6c9d41952b977b64978a1fde8224bf677c395
solean <he15man2007@gmail.com>
2016-05-29 17:30:00 -0400

    Module now grabs the statuses of Xbox Live's 5 services.

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 const XBL_STATUS_URL = 'http://support.xbox.com/en-US/xbox-live-status';
+const SERVICES = ['XboxLiveCoreServices', 'PurchaseandContentUsage',
+									'Website', 'TVMusicandVideo', 'SocialandGaming'];
 
-function getHtml() {
+// Statuses: 'Normal', 'Limited', 'Unavailable', 'Unknown'
+
+function getXblServicesHtml() {
 	return new Promise((resolve, reject) => {
 		request(XBL_STATUS_URL, function(error, response, body) {
 			if (!error && response.statusCode == 200) {
@@ -15,8 +19,22 @@ function getHtml() {
 	});
 }
 
-getHtml().then(function(html) {
-	var $ = cheerio.load(html);
-}).catch(function(error) {
-	console.log(error);
-});
+function getServiceStatuses(page) {
+	var $ = cheerio.load(page);
+	var output = {};
+
+	SERVICES.map((serviceName) => {
+		output[serviceName] = $('#' + serviceName + ' .statusheading span').html()
+	});
+	return output;
+}
+
+function main() {
+	getXblServicesHtml().then(function(html) {
+		console.log(getServiceStatuses(html));
+	}).catch(function(error) {
+		console.log(error);
+	});
+}
+
+main();
