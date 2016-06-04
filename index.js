45aeb18af7196b4969352ce4d425eaf49065398c
solean <he15man2007@gmail.com>
2016-06-04 19:18:02 -0400

    Print statuses to the console.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var chalk = require('chalk');
 
 const XBL_STATUS_URL = 'http://support.xbox.com/en-US/xbox-live-status';
 const SERVICES = ['XboxLiveCoreServices', 'PurchaseandContentUsage',
 									'Website', 'TVMusicandVideo', 'SocialandGaming'];
 
-// Statuses: 'Normal', 'Limited', 'Unavailable', 'Unknown'
 
-function getXblServicesHtml() {
+function getXboxLiveServicesHtml() {
 	return new Promise((resolve, reject) => {
 		request(XBL_STATUS_URL, function(error, response, body) {
 			if (!error && response.statusCode == 200) {
@@ -29,9 +29,35 @@ function getServiceStatuses(page) {
 	return output;
 }
 
+function printPrettyServiceStatuses(serviceStatusMap) {
+	var colorStatus = (status) => {
+		var output = status;
+		switch(status) {
+			case 'Normal':
+				output = chalk.green(status);
+				break;
+			case 'Limited':
+				output = chalk.yellow(status);
+				break;
+			case 'Unavailable':
+				output = chalk.red(status);
+				break;
+			default:
+				output = chalk.gray('Unknown');
+				break;
+		}
+		return output;
+	};
+
+	Object.keys(serviceStatusMap).forEach((service) => {
+		console.log(chalk.bold(service) + ': ' + colorStatus(serviceStatusMap[service]));
+	});
+}
+
 function main() {
-	getXblServicesHtml().then(function(html) {
-		console.log(getServiceStatuses(html));
+	getXboxLiveServicesHtml().then(function(html) {
+		var services = getServiceStatuses(html);
+		printPrettyServiceStatuses(services);
 	}).catch(function(error) {
 		console.log(error);
 	});
