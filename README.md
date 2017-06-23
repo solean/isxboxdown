1b880c46ff0aca84951cbcf514a56385a3b2896f
Chris Schnabel <chrisjschnabel@gmail.com>
2017-06-23 14:42:10 -0400

    Some refactoring

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # isxboxdown
 
 Node module that parses Xbox Live's Service Status page.
+
+Installation:
+- clone this repo
+- run "npm install" and then run "npm install -g"
+- now you should be able to run the command "isxboxdown"
--- index.js
+++ index.js
@@ -1,61 +1,13 @@
 #!/usr/bin/env node
 
-const request = require('request');
-const cheerio = require('cheerio');
-const utils = require('./utils');
-
-const XBL_STATUS_URL = 'http://support.xbox.com/en-US/xbox-live-status';
-const SERVICE_NAMES = ['Xbox Live Core Services',
-											 'Purchase and Content Usage',
-											 'Website',
-											 'TV Music and Video',
-											 'Social and Gaming'
-											];
-
-
-function getHTML(url) {
-	return new Promise((resolve, reject) => {
-		request(url, (error, response, body) => {
-			if (!error && response.statusCode == 200) {
-				resolve(body);
-			} else {
-				reject(error);
-			}
-		});
-	});
-}
-
-function getServiceStatusesFromHtml(page) {
-	var $ = cheerio.load(page);
-	var output = {};
-
-	SERVICE_NAMES.map(serviceName => {
-		var className = serviceName.split(' ').join('');
-		output[serviceName] = $('#' + className + ' .statusheading span').html()
-	});
-	return output;
-}
-
-function getServiceStatuses() {
-	return new Promise((resolve, reject) => {
-		getHTML(XBL_STATUS_URL).then(html => {
-			resolve(getServiceStatusesFromHtml(html));
-		}).catch(error => {
-			reject('Sorry, an error occurred:\n' + '"' + error + '"');
-		})
-	});
-}
-
+const isXboxDown = require('./lib/isxboxdown');
+const utils = require('./lib/utils');
 
 function main() {
-	getServiceStatuses().then(services => {
-		utils.printPrettyServiceStatuses(services);
-	}).catch(error => {
-		console.log(error);
-	})
+  isXboxDown.getServiceStatuses()
+    .then(utils.printPrettyServiceStatuses)
+    .catch(console.log);
 }
 
-module.exports.getServiceStatuses = getServiceStatuses;
 
 main();
-
